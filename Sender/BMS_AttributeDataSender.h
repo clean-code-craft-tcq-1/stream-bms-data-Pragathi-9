dfc36644b652e0b62989597e9b7267c244fbc34f
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-05-31 22:08:03 +0530

    Update BMS_AttributeDataSender.h

--- Sender/BMS_AttributeDataSender.h
+++ Sender/BMS_AttributeDataSender.h
@@ -4,15 +4,15 @@ typedef enum {
   Failure,
   Success
 } OperationMode;
-
 typedef enum {
   StopData,
   SendData 
 } DataTransmitMode;
 
-
-
 OperationMode BMS_Readfromdatafile();
 OperationMode BMS_WriteToConsole(DataTransmitMode UserTransmitRequest);
-
+extern int BufferArraysize;
+extern float Temperature[MAX_READINGS];
+extern float ChargeRate[MAX_READINGS];
+OperationMode ReadStatus, WriteStatus;
 
